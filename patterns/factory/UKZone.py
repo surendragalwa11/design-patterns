b047fe6a2273d108ffae2fde974fac1d28990f4c
Surendra Galwa <surendragalwa111@gmail.com>
2020-05-22 15:55:37 +0530

    Factory: Added abstract method and inheritance

--- patterns/factory/UKZone.py
+++ patterns/factory/UKZone.py
@@ -1,16 +1,5 @@
-class UKZone:
+from factory import Factory
+
+class UKZone(Factory):
     def __init__(self):
-        self.__confidence = 8
-        self.__threshold = 7
-
-    def setConfidence(self, val):
-        self.__confidence = val
-    
-    def getConfidence(self):
-        return self.__confidence
-
-    def setThreshold(self, val):
-        self.__threshold = val
-    
-    def getThreshold(self):
-        return self.__threshold
+        Factory.__init__(self, 8, 7)
--- patterns/factory/index.py
+++ patterns/factory/index.py
@@ -6,16 +6,20 @@ class ZoneFactory:
     def __init__(self, zone):
         self.zone = zone
 
+    def getFactoryConfig(self):
+        return {
+            'ASIA': AsiaZone(),
+            'US': USZone(),
+            'UK': UKZone()
+        }
+
     def getZone(self):
-        if self.zone == 'ASIA':
-            return AsiaZone()
-        elif self.zone == 'US':
-            return USZone()
-        else:
-            return UKZone()
+        config = self.getFactoryConfig()
+        zone = config[self.zone]
+        return zone
 
 if __name__ == '__main__':
-    zone = ZoneFactory('ASIA').getZone()
+    zone = ZoneFactory('ASI').getZone()
     confidence = zone.getConfidence()
     threshold = zone.getThreshold()
     print(confidence, threshold)
